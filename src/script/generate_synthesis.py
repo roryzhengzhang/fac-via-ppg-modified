e721c653f7d32597e0fa3ab03463b20ef453842c
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-08-01 17:46:52 -0700

    update

--- src/script/generate_synthesis.py
+++ src/script/generate_synthesis.py
@@ -87,9 +87,9 @@ if __name__ == '__main__':
 
     if os.path.isfile(teacher_utt_path):
         logging.info('Perform AC on %s', teacher_utt_path)
-        teacher_ppg, alignments = get_ppg(teacher_utt_path, deps)
+        teacher_ppg = get_ppg(teacher_utt_path, deps)
         print(f"teacher_ppg size: {teacher_ppg.size()}")
-        ac_mel = get_inference(teacher_ppg, tacotron_model, is_clip)
+        ac_mel, alignments = get_inference(teacher_ppg, tacotron_model, is_clip)
         print(f"mel size: {ac_mel.size()}")
         ac_wav = waveglow_audio(ac_mel, waveglow_model,
                                 waveglow_sigma, True)
