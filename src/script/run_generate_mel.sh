4e55f0368e94d939a9fd3e54647ab4693d2b3a42
roryzhengzhang <roryzhang95@gmail.com>
2022-08-02 22:41:50 +0000

    update

---
+++ src/script/run_generate_mel.sh
@@ -0,0 +1,5 @@
+python generate_mel.py \
+--ppg2mel_model /home/zheng_zhang7/fac-via-ppg/interspeech19-stage/ppg2speech-si-am-si-tacotron-bdl2ykwk-final/tacotron_checkpoint_11000 \
+--waveglow_model /home/zheng_zhang7/fac-via-ppg/interspeech19-stage/ppg2speech-si-am-si-tacotron-bdl2ykwk-final/waveglow_270000 \
+--teacher_utterance_path /home/zheng_zhang7/fac-via-ppg/teacher-utterance/arctic_a0016.wav \
+--output_dir /home/zheng_zhang7/fac-via-ppg/aaron_syn_output/
