d445bdbdbc2ad7a265b3b38e16c1d3109c023817
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-08-03 17:01:44 -0700

    update

--- src/script/common/hparams.py
+++ src/script/common/hparams.py
@@ -58,7 +58,7 @@ def create_hparams(**kwargs):
         # Directory to save tensorboard logs. Just keep it like this.
         "log_directory": 'log',
         'data_dir': '/home/zheng_zhang7/seq2seq_accent_conversion_model',
-        "checkpoint_path": '/home/zheng_zhang7/fac-via-ppg/interspeech19-stage/ppg2speech-si-am-si-tacotron-bdl2ykwk-final/tacotron_checkpoint_11000',  # Path to a checkpoint file.
+        # "checkpoint_path": '/home/zheng_zhang7/fac-via-ppg/interspeech19-stage/ppg2speech-si-am-si-tacotron-bdl2ykwk-final/tacotron_checkpoint_11000',  # Path to a checkpoint file.
         "warm_start": False,  # Load the model only (warm start)
         "n_gpus": 1,  # Number of GPUs
         "rank": 0,  # Rank of current gpu
